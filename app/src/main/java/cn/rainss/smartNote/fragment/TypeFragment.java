b246726a1b6dc6dd477b94a4effd4700f132a46a
rainerosion <rainerosion@qq.com>
2020-04-22 01:08:42 +0800

    :lipstick: type management finished!

--- app/src/main/java/cn/rainss/smartNote/fragment/TypeFragment.java
+++ app/src/main/java/cn/rainss/smartNote/fragment/TypeFragment.java
@@ -3,14 +3,16 @@ package cn.rainss.smartNote.fragment;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -21,6 +23,11 @@ import com.xuexiang.xui.adapter.recyclerview.XLinearLayoutManager;
 import com.xuexiang.xui.utils.ResUtils;
 import com.xuexiang.xui.widget.dialog.materialdialog.DialogAction;
 import com.xuexiang.xui.widget.dialog.materialdialog.MaterialDialog;
+import com.yanzhenjie.recyclerview.OnItemMenuClickListener;
+import com.yanzhenjie.recyclerview.SwipeMenu;
+import com.yanzhenjie.recyclerview.SwipeMenuBridge;
+import com.yanzhenjie.recyclerview.SwipeMenuCreator;
+import com.yanzhenjie.recyclerview.SwipeMenuItem;
 import com.yanzhenjie.recyclerview.SwipeRecyclerView;
 
 import java.text.ParseException;
@@ -31,24 +38,17 @@ import java.util.List;
 
 import butterknife.BindView;
 import cn.rainss.smartNote.R;
-import cn.rainss.smartNote.adapter.NewsCardViewListAdapter;
 import cn.rainss.smartNote.adapter.TypeItemViewAdapter;
 import cn.rainss.smartNote.adapter.entity.Type;
 import cn.rainss.smartNote.core.BaseFragment;
 import cn.rainss.smartNote.utils.SQLiteUtils;
 import cn.rainss.smartNote.utils.XToastUtils;
-import cn.rainss.smartNote.widget.MaterialLoadMoreView;
 
-@Page(name = "分类")
+@Page(name = "分类管理")
 public class TypeFragment extends BaseFragment {
     /**
      * 绑定页面元素
      */
-    /**
-     * 顶部数据
-     */
-//    @BindView(R.id.toolbar_recycler_view)
-//    Toolbar toolbar;
     @BindView(R.id.recyclerView)
     SwipeRecyclerView recyclerView;
     @BindView(R.id.swipe_refresh_layout)
@@ -77,6 +77,9 @@ public class TypeFragment extends BaseFragment {
         recyclerView.setLayoutManager(new XLinearLayoutManager(recyclerView.getContext()));
         //设置动画
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setSwipeMenuCreator(swipeMenuCreator);
+        //设置操作菜单点击事件
+        recyclerView.setOnItemMenuClickListener(mMenuItemClickListener);
         recyclerView.setAdapter(mAdapter = new TypeItemViewAdapter());
         swipeRefreshLayout.setColorSchemeColors(0xff0099cc, 0xffff4444, 0xff669900, 0xffaa66cc, 0xffff8800);
     }
@@ -209,4 +212,55 @@ public class TypeFragment extends BaseFragment {
         }
         return types;
     }
+    /**
+     * 菜单创建器，在Item要创建菜单的时候调用。
+     */
+    private SwipeMenuCreator swipeMenuCreator = new SwipeMenuCreator() {
+        @Override
+        public void onCreateMenu(SwipeMenu swipeLeftMenu, SwipeMenu swipeRightMenu, int position) {
+            // 设置按钮的宽度和高度
+            int width = getResources().getDimensionPixelSize(R.dimen.dp_55);
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            // 添加右侧操作
+            {
+                SwipeMenuItem deleteItem = new SwipeMenuItem(getActivity()).setBackground(R.drawable.selector_red)
+                        .setImage(R.drawable.ic_action_delete)
+                        .setText(ResUtils.getString(R.string.delete))
+                        .setTextColor(Color.WHITE)
+                        .setWidth(width)
+                        .setHeight(height);
+                swipeRightMenu.addMenuItem(deleteItem);// 添加菜单到右侧。
+            }
+        }
+    };
+
+    /**
+     * RecyclerView的Item的Menu点击监听。
+     */
+    private OnItemMenuClickListener mMenuItemClickListener = new OnItemMenuClickListener() {
+        @Override
+        public void onItemClick(SwipeMenuBridge menuBridge, int position) {
+            menuBridge.closeMenu();
+
+            int direction = menuBridge.getDirection(); // 左侧还是右侧菜单。
+            int menuPosition = menuBridge.getPosition(); // 菜单在RecyclerView的Item中的Position。
+
+            if (direction == SwipeRecyclerView.RIGHT_DIRECTION) {
+                View viewByPosition = recyclerView.getLayoutManager().findViewByPosition(position);
+                //获取待删除分类的id
+                TextView typeIdView = (TextView) viewByPosition.findViewById(R.id.type_id);
+                String id = typeIdView.getText().toString();
+                //获取sqlite对象
+                SQLiteUtils sqLiteUtils = new SQLiteUtils(getActivity());
+                SQLiteDatabase db = sqLiteUtils.getWritableDatabase();
+                if(id != null && !id.trim().equals("")){
+                    db.delete("type","id = ? ",new String[]{ id });
+                }
+                db.close();
+                sqLiteUtils.close();
+                //刷新数据
+                refresh();
+            }
+        }
+    };
 }
